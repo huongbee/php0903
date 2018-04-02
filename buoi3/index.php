1cae9a5aa029ede67a75e2849231405b342c7516
Huong <admin@Apples-MacBook-Pro.local>
2018-04-02 20:00:08 +0700

    data

--- buoi3/index.php
+++ buoi3/index.php
@@ -14,156 +14,52 @@
 
     </div>
     <div class="content">
+        <?php 
+        // include('data.php');
+        include_once('data.php');
 
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F51</p>
-                <p class="price">6.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F52</p>
-                <p class="price">7.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F53</p>
-                <p class="price">8.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F54</p>
-                <p class="price">9.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F5</p>
-                <p class="price">6.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">
-                    <b>OPPO F5</b>
-                </p>
-                <p class="price">
-                    <strong>6.990.000 vnđ</strong>
-                </p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F5</p>
-                <p class="price">6.990.000 vnđ</p>
-            </div>
-        </div>
+        //require 'data.php';
+        //require_once 'data2.php';
 
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
+        foreach($arrSanpham as $sanpham):
+        ?>
+            <div class="item">
+                <div class="image">
+                    <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
+                </div>
+                <div class="info">
+                    <h3>OPPO F5</h3>
+                    <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
+                    <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
+                    <p class="promotion">Khuyến mãi: ....</p>
+                    <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
+                </div>
+                <div class="footer">
+                    <p class="name">OPPO F51</p>
+                    <p class="price">6.990.000 vnđ</p>
+                </div>
             </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F5</p>
-                <p class="price">6.990.000 vnđ</p>
-            </div>
-        </div>
-        <div class="item">
-            <div class="image">
-                <img src="images/oppo-f5-red-ah-400x460.png" alt="OPPO F5">
-            </div>
-            <div class="info">
-                <h3>OPPO F5</h3>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-                <p class="promotion">Khuyến mãi: ....</p>
-                <p>Phiếu mua hàng trị giá 100.000đ khi mua online</p>
-            </div>
-            <div class="footer">
-                <p class="name">OPPO F5</p>
-                <p class="price">6.990.000 vnđ</p>
-            </div>
-        </div>
+        <?php endforeach  ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script>
